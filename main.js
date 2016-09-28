ffe435e69c4b46524c13d48ed3450b512021d7e1
Benjamin Landers <benjaminrlanders@gmail.com>
2016-09-27 22:33:20 -0700

    Modified display to rescale
    Top 1% and bottom 1% are clamped and the remaining values are spread across the whole range.

--- main.js
+++ main.js
@@ -10,6 +10,46 @@ function hist(n,sort){
   }
   return h;
 }
+function bounds(hist2){
+  var total = 0, s = sum(hist2);
+  var a, b;
+  for(var i = 0; i < hist2.length;i++){
+    total += hist2[i];
+    if(total > s/100){
+      a = i-1;
+      break;
+    }
+  }
+  total = 0;
+  for(var i = hist2.length-1; i >= 0;i--){
+    total += hist2[i];
+    if(total > s/100){
+      b = i+1;
+      break;
+    }
+  }
+  return [a,b];
+}
+function computeHist2(hist){
+  var m = max(0,hist);
+  
+  var hist2 = new Array(1200).fill(0);
+  for(var i = 0; i < hist.length; i++){
+    for(var k = 0; k < hist[0].length;k++){
+      let hue = Math.floor(1200*Math.sqrt(hist[i][k])/Math.sqrt(m));
+
+      if(hue >= 1199){
+        hue = 1199;
+      }
+
+      hist2[hue]++;
+    }
+  }
+  return hist2;
+}
+var sum = (a)=>a.reduce((a,b)=>a+b,0);
+
+
 
 function display(hist){
   var c = document.createElement('canvas');
@@ -19,25 +59,29 @@ function display(hist){
   var m = max(0,hist);
   var a = 300/hist.length, b = 300/hist[0].length;
   
-  var hist2 = new Array(360).fill(0);
+
+  
+  var hist2 = computeHist2(hist);           //computes histogram of pixels hues vs count
+  var [lower,upper] = bounds(hist2);        //computes bounds such that 98% of pixels don't get clipped
+  var hist3 = new Array(300).fill(0);
   for(var i = 0; i < hist.length; i++){
     for(var k = 0; k < hist[0].length;k++){
-      let hue = Math.floor(300*hist[i][k]/m);
+      let hue = 1200*Math.sqrt(hist[i][k])/Math.sqrt(m);
 
-      if(hue > 299){
-        hue = 299;
-      }
+      hue = Math.floor(300/(upper-lower)*(hue - lower));//rescale so the lower bound = 0 and upper bound = 300 and sample with dx=1
+      hue = Math.min(Math.max(hue,0),299);  //clamp to 0-299
 
-      hist2[hue]++;
-      ctx.fillStyle = 'hsl(' + hue + ',100%,50%)'
+      hist3[hue]++;
+
+      ctx.fillStyle = 'hsl(' + hue + ',100%,50%)';
       ctx.fillRect(i*a,k*b,a,b);
     }
   }
   
-  m = max(0,hist2);
+  m = max(0,hist3);
   for(var i = 0; i < 360; i++){
     ctx.fillStyle = 'hsl(' + i + ',100%,50%)'
-    ctx.fillRect(i,400-Math.floor(100*hist2[i]/m),1,Math.floor(100*hist2[i]/m));
+    ctx.fillRect(i,400-Math.floor(100*hist3[i]/m),1,Math.floor(100*hist3[i]/m));
   }
   return c.toDataURL();
 }
