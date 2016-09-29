0f855bcce8ff3b3cef325c2b4d290b2dc0f2a5d9
Benjamin Landers <benjaminrlanders@gmail.com>
2016-09-28 22:00:15 -0700

    Add mergesort and heapsort
    Also adds quickInsertSort which is a quicksort until length < 10 then it switches to insertion sort.
    
    Also adding that required slightly changing the custom insertion sort for shell sort .

--- sorts.js
+++ sorts.js
@@ -44,14 +44,14 @@ function shellSort(arr,comp){
   for(; h < arr.length; h = 3*h+1);
   for(; h > 0; h = Math.floor(h / 3)){
     for(var i = 0; i < h; i ++){
-      shellInsert(arr,comp,i,h);
+      insertCustom(arr,comp,i,h,arr.length);
     }
   }
 }
 
-//insertion sort with custom increment and initial position
-function shellInsert(arr,comp,start,increment){
-  for(var i = start+increment; i < arr.length; i+=increment){
+//insertion sort with custom increment, initial position and end position
+function insertCustom(arr,comp,start,increment,end){
+  for(var i = start+increment; i < end; i+=increment){
     for(var k = i; k - increment >= 0; k -= increment){
       if(comp(arr[k-increment],arr[k]) > 0){
         let temp = arr[k];
@@ -91,4 +91,92 @@ function partition(arr,comp,lo,hi){
   arr[hi] = arr[k];
   arr[k]  = pivot;
   return k;
+}
+
+function mergeSort(arr,comp){
+  return mergeSortRecurse(arr,comp,0,arr.length-1,0);
+}
+
+function mergeSortRecurse(arr,comp,start,end,l){
+  if(start >= end){
+    return;
+  }
+  var a = Math.floor(end/2+start/2);
+  mergeSortRecurse(arr,comp,start,a,l+1);
+  mergeSortRecurse(arr,comp,a+1,end,l+1);
+
+  // merge
+  var tmp = arr.slice(start,a+1); // move first elements out of the way for the merge.
+  var i = 0, k = a+1;             // indexes for subarrays
+  for(var j = start; j < end+1;j++){
+    if(i > a-start){              // if the first subarray is empty
+      arr[j] = arr[k];
+      k++;
+    }else if(k > end){            // if the second subarray is empty
+      arr[j] = tmp[i];
+      i++;
+    }else{
+      if(comp(tmp[i],arr[j])>0){  // otherwise compare and move the smaller one
+        arr[j] = arr[k];
+        k++;
+      }else{
+        arr[j] = tmp[i];
+        i++;
+      }
+    }
+  }
+}
+
+
+function quickInsertSort(arr,comp){
+  return quickInsertSortRecurse(arr,comp,0,arr.length-1)  
+}
+
+//quicksort on a slice of the array
+function quickInsertSortRecurse(arr,comp,lo,hi){
+  if(lo < hi)
+    if(lo + 10 < hi){ //if sub array > 10 elements, partition otherwise insert
+      let pivot = partition(arr,comp,lo,hi);
+      quickInsertSortRecurse(arr,comp,lo,pivot-1);
+      quickInsertSortRecurse(arr,comp,pivot+1,hi);
+    }else{
+      insertCustom(arr,comp,lo,1,hi+1);
+    }
+}
+
+
+function heapSort(arr, comp){
+    heapify(arr, comp)
+    for(var i = arr.length-1; i > 0;i--){
+      let t = arr[i];
+      arr[i] = arr[0];
+      arr[0] = t;
+      siftDown(arr, comp, 0, i-1)
+    }
+}
+function heapify(arr, comp){
+    for(var i = Math.floor(((arr.length-1)-1)/2); i >= 0; i--){
+        siftDown(arr, comp, i, arr.length - 1);
+    }
+}
+function siftDown(arr, comp, root, end){
+    while(2*root + 1 <= end){
+        let child = 2*root+1;   
+        let tmp = root;           
+
+        if(comp(arr[child],arr[tmp])>0){
+            tmp = child;
+        }
+        if(child+1 <= end && comp(arr[child+1],arr[tmp])>0){
+            tmp = child + 1;
+        }
+        if(tmp  == root){
+            return;
+        }else{
+            let t = arr[root];
+            arr[root] = arr[tmp];
+            arr[tmp] = t;
+            root = tmp;
+        }
+    }
 }
